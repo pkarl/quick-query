0f676993e1ad1647c194fca9ccd52d70ef1d99f3
Pete Karl II <pete@upstatement.com>
2014-06-27 13:49:58 -0400

    teaked plugin slug

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,7 +9,7 @@
 
 // Activates this plugin in WordPress so it can be tested.
 $GLOBALS['wp_tests_options'] = array(
-	'active_plugins' => array( 'QuickQuery/QuickQuery.php' ),
+	'active_plugins' => array( 'quick-query/QuickQuery.php' ),
 );
 
 // If the develop repo location is defined (as WP_DEVELOP_DIR), use that
