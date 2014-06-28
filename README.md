4680353496e783f56dc3237247a41a178af60943
Pete Karl II <pete@upstatement.com>
2014-06-28 07:27:58 -0400

    more travis tinkering to get builds testin properly

--- README.md
+++ README.md
@@ -38,3 +38,11 @@ $posts = $q->type('post')->tags( ['movie_genre' => ['action','comedy'], 'actor' 
 I'm working on creating an honest-to-goodness open source version of it from the ground up. At the time of this writing, Quick Query is just a class wrapper with all of our code commented out. 
 
 I'll be working on this (along with anyone who enjoys the idea of mitigating the emotional damage WP_Query causes) to incrementally re-add the functionality of `QQuery` along with tests and docs as we go.
+
+# Running Tests
+
+I'm just going to document what I went through to get a local test env up in OSX.
+
+I'm following this first: https://github.com/Homebrew/homebrew-php to get PHP + Pear + PHPUnit
+
+...then this: http://michaelheap.com/installing-phpunit-under-php-5-4-from-homebrew/
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,23 +1,18 @@
 <?php
-/**
- * Bootstrap the plugin unit testing environment.
- *
- * Edit 'active_plugins' setting below to point to your main plugin file.
- *
- * @package wordpress-plugin-tests
- */
+
+$_tests_dir = getenv('WP_TESTS_DIR');
+if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+
+require_once $_tests_dir . '/includes/functions.php';
+
+function _manually_load_plugin() {
+	require dirname( __FILE__ ) . '/../QuickQuery.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+require $_tests_dir . '/includes/bootstrap.php';
 
 // Activates this plugin in WordPress so it can be tested.
-$GLOBALS['wp_tests_options'] = array(
-	'active_plugins' => array( 'quick-query/QuickQuery.php' ),
-);
-
-// If the develop repo location is defined (as WP_DEVELOP_DIR), use that
-// location. Otherwise, we'll just assume that this plugin is installed in a
-// WordPress develop SVN checkout.
-
-if( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-	require getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit/includes/bootstrap.php';
-} else {
-	require '../../../../tests/phpunit/includes/bootstrap.php';
-}
+// $GLOBALS['wp_tests_options'] = array(
+// 	'active_plugins' => array( 'quick-query/QuickQuery.php' ),
+// );
