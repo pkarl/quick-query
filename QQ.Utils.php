5cc73ac4b74f6a011caffe761e4b3157f76da3b6
Pete Karl II <pete@upstatement.com>
2014-07-04 19:12:52 -0400

    fix: a > in QQ.Utils.php

--- QQ.Utils.php
+++ QQ.Utils.php
@@ -14,7 +14,7 @@ Class QQ_Utils {
 			'EMPTY_SET' => 'QQuery has received data that will return an empty set',
 			'ARR_CONVERSION' => 'QQuery is attempting to convert an unusual object to an array',
 			'EMPTY_ARR' => 'QQuery has found or generated an empty array',
-			'NON_OBJECT_RETURNED' = 'WP_Post has been requested, but WP did not return one'
+			'NON_OBJECT_RETURNED' => 'WP_Post has been requested, but WP did not return one'
 		);
 
 		trigger_error( $qq_warnings[$warning_code], E_USER_WARNING);
